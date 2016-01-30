60646811801da5a90a9261b418d0539e7970893b
Dominik Broj <dominik.broj@adesso.de>
2016-01-30 13:59:09 +0100

    Initial creation

---
+++ src/main/java/de/adesso/azubis/iliasdownloader/v3/Main.java
@@ -0,0 +1,21 @@
+package de.adesso.azubis.iliasdownloader.v3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Dominik Broj on 30.01.2016.
+ *
+ * @author Dominik Broj
+ * @since 30.01.2016
+ */
+public final class Main extends Application {
+	public static void main(String[] args) {
+		Main.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+}
