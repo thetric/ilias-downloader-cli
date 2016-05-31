92a052da49380cda3f03ec0eba29ec58198d8423
Dominik Broj <dominik.broj@adesso.de>
2016-05-31 21:14:20 +0200

    added web ilias srvc

---
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/impl/webparser/WebIliasService.java
@@ -0,0 +1,20 @@
+package de.adesso.iliasdownloader3.service.impl.webparser;
+
+import de.adesso.iliasdownloader3.service.IliasService;
+import de.adesso.iliasdownloader3.service.model.LoginCredentials;
+
+/**
+ * @author broj
+ * @since 31.05.2016
+ */
+public final class WebIliasService implements IliasService {
+    @Override
+    public void login(LoginCredentials loginCredentials) {
+        
+    }
+
+    @Override
+    public void logout() {
+
+    }
+}
