b3b6f0fa5fe58964e8b2b6a3589ef1b7ed0315a2
Dominik Broj <dominik.broj@adesso.de>
2016-08-18 12:10:09 +0200

    add logging to web ilias srvc

--- ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/impl/webparser/WebIliasService.java
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/impl/webparser/WebIliasService.java
@@ -6,6 +6,7 @@ import de.adesso.iliasdownloader3.service.model.Course;
 import de.adesso.iliasdownloader3.service.model.LoginCredentials;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.jsoup.Connection;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
@@ -24,6 +25,7 @@ import java.util.stream.Collectors;
  * @since 31.05.2016
  */
 @RequiredArgsConstructor
+@Log4j2
 public final class WebIliasService implements IliasService {
     private static final String ILIAS_BASE_URL = "https://www.ilias.fh-dortmund.de/ilias/";
     // TODO externalize base URL
@@ -40,6 +42,7 @@ public final class WebIliasService implements IliasService {
     @Override
     public void login(LoginCredentials loginCredentials) {
         Response response;
+        log.info("Loggig in at {}", LOGIN_PAGE);
         try {
             response = Jsoup
                     .connect(LOGIN_PAGE)
@@ -48,8 +51,10 @@ public final class WebIliasService implements IliasService {
                     .method(Connection.Method.POST)
                     .execute();
         } catch (IOException e) {
+            log.error("Login at " + LOGIN_PAGE + " failed", e);
             throw exceptionTranslator.translate(e);
         }
+        log.info("Login at {} succeeded", LOGIN_PAGE);
 
         ensureAuthentication(response);
         cookies = response.cookies();
@@ -72,13 +77,17 @@ public final class WebIliasService implements IliasService {
         // hat den Effekt, dass das Cookie "ilClientId" gelöscht wird
         // wenn wieder der Login aufgerufen wird, hat der Client noch das "authchallenge" Cookie
         // ist der Logout noch nötig?
+        log.info("Logging out: {}", LOGOUT_PAGE);
+
         try {
             Response response = connectWithSessionCookies(LOGOUT_PAGE)
                     .method(Connection.Method.GET)
                     .execute();
         } catch (IOException e) {
+            log.error("Logout at " + LOGOUT_PAGE + " failed", e);
             throw exceptionTranslator.translate(e);
         }
+        log.info("Logout at {} succeeded", LOGOUT_PAGE);
     }
 
     private Connection connectWithSessionCookies(String iliasWebsite) {
@@ -92,29 +101,39 @@ public final class WebIliasService implements IliasService {
         Response response;
         Document document;
 
+        log.info("Get all courses and groups from {}", COURSES_AND_GROUPS_OVERVIEW);
         try {
             response = connectWithSessionCookies(COURSES_AND_GROUPS_OVERVIEW)
                     .execute();
             document = response.parse();
         } catch (IOException e) {
+            log.error("Could not get courses/groups from " + COURSES_AND_GROUPS_OVERVIEW, e);
             throw exceptionTranslator.translate(e);
         }
 
+        log.info("Mapping course/groups to entities...");
+        Collection<Course> courses = getCoursesFromHtml(document);
+        log.debug("Found courses: {}", courses);
+        return courses;
+    }
+
+    private Collection<Course> getCoursesFromHtml(Document document) {
         String courseLinkCssSelector = "a[href*='_crs_'].il_ContainerItemTitle";
         Elements elements = document.select(courseLinkCssSelector);
 
         return elements.stream().map(aTag -> {
             int courseId = getCourseId(aTag);
             String courseName = aTag.text();
-            return new Course(courseId, courseName);
+            String courseUrl = aTag.attr("href");
+            return new Course(courseId, courseName, courseUrl);
         }).collect(Collectors.toList());
     }
 
     private int getCourseId(Element aTag) {
         String href = aTag.attr("href");
-        // href="http://www.ilias.fh-dortmund.de/ilias/goto_ilias-fhdo_crs_XXXX.html"
+        // href="http://www.ilias.fh-dortmund.de/ilias/goto_ilias-fhdo_crs_\d+.html"
         String idString = href
-                .replaceFirst("http://www.ilias.fh-dortmund.de/ilias/goto_ilias-fhdo_crs_", "")
+                .replaceFirst("https://www.ilias.fh-dortmund.de/ilias/goto_ilias-fhdo_crs_", "")
                 .replace(".html", "");
         // der Rest muss ein int sein
         return Integer.parseInt(idString);
