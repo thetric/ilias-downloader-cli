8e76b61b732e4303c12d3e09fcef10032c8bf812
Dominik Broj <dominik.broj@adesso.de>
2016-08-18 15:23:58 +0200

    add children to course folder

--- ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/CourseFolder.java
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/CourseFolder.java
@@ -1,8 +1,19 @@
 package de.adesso.iliasdownloader3.service.model;
 
+import lombok.Getter;
+
+import java.util.Collection;
+
 /**
  * @author broj
  * @since 18.08.2016
  */
 public final class CourseFolder extends AbstractIliasItem implements CourseItem {
+    @Getter
+    private final Collection<? extends CourseItem> courseItems;
+
+    public CourseFolder(int id, String name, String url, Collection<? extends CourseItem> courseItems) {
+        super(id, name, url);
+        this.courseItems = courseItems;
+    }
 }
