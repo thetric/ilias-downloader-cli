fd1996ec910c6e7d890f70ab99384c503174eedd
Dominik Broj <dominik.broj@adesso.de>
2016-08-18 12:37:34 +0200

    add superclass for ilias tree objects

---
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/AbstractIliasItem.java
@@ -0,0 +1,18 @@
+package de.adesso.iliasdownloader3.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author broj
+ * @since 18.08.2016
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class AbstractIliasItem {
+    private int id;
+    private String name;
+    private String url;
+}
