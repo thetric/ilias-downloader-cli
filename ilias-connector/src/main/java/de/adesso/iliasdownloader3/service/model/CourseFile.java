8ab73c1dc216f0e7fb2319e048a91f73ee95d165
Dominik Broj <dominik.broj@adesso.de>
2016-08-18 15:22:35 +0200

    add missing ctor in CourseFile

--- ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/CourseFile.java
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/CourseFile.java
@@ -5,4 +5,7 @@ package de.adesso.iliasdownloader3.service.model;
  * @since 18.08.2016
  */
 public final class CourseFile extends AbstractIliasItem implements CourseItem {
+    public CourseFile(int id, String name, String url) {
+        super(id, name, url);
+    }
 }
