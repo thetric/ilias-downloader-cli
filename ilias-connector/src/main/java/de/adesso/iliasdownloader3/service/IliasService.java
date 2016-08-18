062f50d3e36be53e4be4bce643baa8b0a1774a2b
Dominik Broj <dominik.broj@adesso.de>
2016-08-18 13:12:16 +0200

    add doc for ilias srvc

--- ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/IliasService.java
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/IliasService.java
@@ -6,6 +6,9 @@ import de.adesso.iliasdownloader3.service.model.LoginCredentials;
 import java.util.Collection;
 
 /**
+ * Provides access to the Ilias.
+ * This interface provides some methods for basic session management (login, logout) and it can list the courses
+ * (with/without their contents).
  * @author broj
  * @since 21.05.2016
  */
@@ -13,11 +16,29 @@ public interface IliasService {
 
     // Session management
 
+    /**
+     * Logs the user in.
+     *
+     * @param loginCredentials
+     *         user credentials
+     * @see #logout()
+     */
     void login(LoginCredentials loginCredentials);
 
+    /**
+     * Logs the current user out.
+     *
+     * @see #login(LoginCredentials)
+     */
     void logout();
 
     // course sync
 
+    /**
+     * Finds all courses without any course content.
+     *
+     * @return all courses of the current user
+     * @see #searchCoursesWithContent()
+     */
     Collection<Course> getJoinedCourses();
 }
