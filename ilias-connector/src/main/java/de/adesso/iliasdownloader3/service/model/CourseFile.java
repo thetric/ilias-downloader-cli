51df5d33e4970f5fdb9790d6ac380ac6456b7d46
Dominik Broj <dominik.broj@adesso.de>
2016-09-24 19:24:10 +0200

    fix toString, equals+hashcode of course file/folder

--- ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/CourseFile.java
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/CourseFile.java
@@ -1,9 +1,14 @@
 package de.adesso.iliasdownloader3.service.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 /**
  * @author broj
  * @since 18.08.2016
  */
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public final class CourseFile extends AbstractIliasItem implements CourseItem {
     public CourseFile(int id, String name, String url) {
         super(id, name, url);
--- ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/CourseFolder.java
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/model/CourseFolder.java
@@ -1,6 +1,9 @@
 package de.adesso.iliasdownloader3.service.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.Collection;
 
@@ -8,6 +11,9 @@ import java.util.Collection;
  * @author broj
  * @since 18.08.2016
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public final class CourseFolder extends AbstractIliasItem implements CourseItem {
     @Getter
     private final Collection<? extends CourseItem> courseItems;
