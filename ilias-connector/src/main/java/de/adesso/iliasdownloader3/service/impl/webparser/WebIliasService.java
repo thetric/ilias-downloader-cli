7af6f4f122324342185bdd8d6ac622011b00e3c0
Dominik Broj <dominik.broj@adesso.de>
2016-09-24 19:26:32 +0200

    web ilias service: deep search for all items should work now

--- ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/impl/webparser/WebIliasService.java
+++ ilias-connector/src/main/java/de/adesso/iliasdownloader3/service/impl/webparser/WebIliasService.java
@@ -2,9 +2,7 @@ package de.adesso.iliasdownloader3.service.impl.webparser;
 
 import de.adesso.iliasdownloader3.service.IliasService;
 import de.adesso.iliasdownloader3.service.exception.IliasAuthenticationException;
-import de.adesso.iliasdownloader3.service.model.Course;
-import de.adesso.iliasdownloader3.service.model.CourseItem;
-import de.adesso.iliasdownloader3.service.model.LoginCredentials;
+import de.adesso.iliasdownloader3.service.model.*;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,13 +14,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.regex.Pattern;
 
-import static de.adesso.iliasdownloader3.service.impl.webparser.CssSelectors.COURSE_SELECTOR;
-import static de.adesso.iliasdownloader3.service.impl.webparser.CssSelectors.GENERIC_ITEM_SELECTOR;
+import static de.adesso.iliasdownloader3.service.impl.webparser.CssSelectors.*;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author broj
@@ -37,6 +34,8 @@ public final class WebIliasService implements IliasService {
     private static final String LOGOUT_PAGE = ILIAS_BASE_URL + "logout.php";
     private static final String COURSES_AND_GROUPS_OVERVIEW = ILIAS_BASE_URL + "ilias.php" +
             "?baseClass=ilPersonalDesktopGUI&cmd=jumpToSelectedItems";
+
+    private static final Pattern ITEM_URL_SPLIT_PATTERN = Pattern.compile("[_.]");
 
     @NonNull
     private final WebIoExceptionTranslator exceptionTranslator;
@@ -77,10 +76,6 @@ public final class WebIliasService implements IliasService {
 
     @Override
     public void logout() {
-        cookies.clear();
-        // hat den Effekt, dass das Cookie "ilClientId" gelöscht wird
-        // wenn wieder der Login aufgerufen wird, hat der Client noch das "authchallenge" Cookie
-        // ist der Logout noch nötig?
         log.info("Logging out: {}", LOGOUT_PAGE);
 
         try {
@@ -90,6 +85,11 @@ public final class WebIliasService implements IliasService {
         } catch (IOException e) {
             log.error("Logout at " + LOGOUT_PAGE + " failed", e);
             throw exceptionTranslator.translate(e);
+        } finally {
+            cookies.clear();
+            // hat den Effekt, dass das Cookie "ilClientId" gelöscht wird
+            // wenn wieder der Login aufgerufen wird, hat der Client noch das "authchallenge" Cookie
+            // ist der Logout noch nötig?
         }
         log.info("Logout at {} succeeded", LOGOUT_PAGE);
     }
@@ -125,14 +125,18 @@ public final class WebIliasService implements IliasService {
     }
 
     private Collection<Course> getCoursesFromHtml(@NonNull Document document) {
-        Elements elements = document.select(COURSE_SELECTOR.getCssSelector());
+        return document
+                .select(COURSE_SELECTOR.getCssSelector())
+                .stream()
+                .map(this::toCourse)
+                .collect(toList());
+    }
 
-        return elements.stream().map(aTag -> {
-            int courseId = getCourseId(aTag);
-            String courseName = aTag.text();
-            String courseUrl = aTag.attr("href");
-            return new Course(courseId, courseName, courseUrl);
-        }).collect(Collectors.toList());
+    private Course toCourse(Element courseElement) {
+        int courseId = getCourseId(courseElement);
+        String courseName = courseElement.text();
+        String courseUrl = courseElement.attr("href");
+        return new Course(courseId, courseName, courseUrl);
     }
 
     private int getCourseId(@NonNull Element aTag) {
@@ -142,22 +146,89 @@ public final class WebIliasService implements IliasService {
                 .replaceFirst(ILIAS_BASE_URL + "goto_ilias-fhdo_crs_", "")
                 .replace(".html", "");
         // der Rest muss ein int sein
-        return Integer.parseInt(idString);
+        return parseId(href, idString);
+    }
+
+    private int parseId(String href, String probableIdString) {
+        try {
+            return Integer.parseInt(probableIdString);
+        } catch (NumberFormatException e) {
+            throw new IliasItemIdStringParsingException(
+                    format("Failed to parse '%s', original string was '%s'", probableIdString, href), e);
+        }
     }
 
     @Override
     public Collection<Course> searchCoursesWithContent(@NonNull Collection<Course> selectedCourses) {
-        return null;
+        return selectedCourses.stream()
+                              .map(this::findCourseItems)
+                              .collect(toList());
     }
 
-    private Course findCourseChildNodes(@NonNull Course course) {
-        Collection<? extends CourseItem> childNodes = searchContentRecursively(course);
+    private Course findCourseItems(@NonNull Course course) {
+        log.debug("Find all children for {}", course.getName());
+        Collection<? extends CourseItem> childNodes = searchItemsRecursively(course.getUrl());
         return new Course(course.getId(), course.getName(), course.getUrl(), childNodes);
     }
 
-    private Collection<? extends CourseItem> searchContentRecursively(Course course) {
-        Document document = connectAndGetDocument(course.getUrl());
-        Elements elements = document.select(GENERIC_ITEM_SELECTOR.getCssSelector());
-        return null;
+    private Collection<? extends CourseItem> searchItemsRecursively(String itemUrl) {
+        return connectAndGetDocument(itemUrl)
+                .select(ITEM_CONTAINER_SELECTOR.getCssSelector())
+                .stream()
+                .map(this::toCourseItem)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(toList());
+    }
+
+    private Optional<CourseItem> toCourseItem(Element itemContainer) {
+        Elements itemTitle = itemContainer.select(ITEM_TITLE_SELECTOR.getCssSelector());
+        String itemName = itemTitle.text();
+        String itemUrl = itemTitle.attr("href");
+        String idString = getItemIdFromUrl(itemUrl);
+        String[] split = ITEM_URL_SPLIT_PATTERN.split(idString);
+
+        String type = split[0];
+        int itemId = Integer.parseInt(split[1]);
+
+        List<String> properties = itemContainer.select(ITEM_PROPERTIES_SELECTOR.getCssSelector())
+                                               .stream()
+                                               .map(Element::text)
+                                               .map(this::trimNbspFromString)
+                                               .map(String::trim)
+                                               .filter(s -> !s.isEmpty())
+                                               .collect(toList());
+
+        return createCourseItem(type, itemId, itemName, itemUrl, properties);
+    }
+
+    /**
+     * Removes the "no backspace" ({@literal &nbsp;} character from the string.
+     *
+     * @param s
+     *         String to trim
+     * @return a new String without "no backspace" characters
+     */
+    private String trimNbspFromString(String s) {
+        return s.replace("\u00a0", "");
+    }
+
+    private String getItemIdFromUrl(String itemUrl) {
+        return itemUrl.replaceFirst(ILIAS_BASE_URL + "goto_ilias-fhdo_", "");
+    }
+
+    private Optional<CourseItem> createCourseItem(@NonNull String type, int itemId, @NonNull String itemName, @NonNull String itemUrl, List<String> properties) {
+        switch (type) {
+            case "fold":
+                Collection<? extends CourseItem> courseItems = searchItemsRecursively(itemUrl);
+                return Optional.of(new CourseFolder(itemId, itemName, itemUrl, courseItems));
+            case "file":
+                log.warn("itemId {}, name {}, url {}", itemId, itemName, itemUrl);
+                String fileType = properties.get(0);
+                return Optional.of(new CourseFile(itemId, itemName + "." + fileType, itemUrl));
+            default:
+                log.warn("Unknown type: {}, URL: {}", type, itemUrl);
+        }
+        return Optional.empty();
     }
 }
