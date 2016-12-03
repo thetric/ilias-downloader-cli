115ad2994868f5b4c66bafc9b25fde2452bd7158
thetric <broj.dominik@gmail.com>
2016-12-03 19:15:19 +0100

    enable sync from main ui (target folder: /tmp/test)

--- jfx-ui/src/main/groovy/com/github/thetric/iliasdownloader/ui/jfx/ui/main/MainUi.groovy
+++ jfx-ui/src/main/groovy/com/github/thetric/iliasdownloader/ui/jfx/ui/main/MainUi.groovy
@@ -1,11 +1,16 @@
 package com.github.thetric.iliasdownloader.ui.jfx.ui.main
 
 import com.github.thetric.iliasdownloader.service.IliasService
+import com.github.thetric.iliasdownloader.service.SyncingIliasItemVisitor
+import com.github.thetric.iliasdownloader.service.model.Course
 import com.github.thetric.iliasdownloader.ui.jfx.ui.util.FxmlLoaderHelper
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView
 import groovy.transform.CompileStatic
+import groovy.util.logging.Log4j2
+import io.reactivex.functions.Consumer
 import javafx.application.HostServices
+import javafx.application.Platform
 import javafx.fxml.FXML
 import javafx.scene.Scene
 import javafx.scene.control.*
@@ -13,6 +18,8 @@ import javafx.scene.image.Image
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 import org.controlsfx.control.ToggleSwitch
+
+import java.nio.file.Paths
 
 import static de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon.*
 
@@ -23,6 +30,7 @@ import static de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon.*
  * @since 05.02.2016
  */
 @CompileStatic
+@Log4j2
 final class MainUi {
     private final Stage stage
     private final BorderPane rootPane
@@ -48,7 +56,7 @@ final class MainUi {
         rootPane = (BorderPane) FxmlLoaderHelper.load(this, '/fxml/mainWindow.fxml')
 
         initGraphics()
-        setEventHandlers(hostServices)
+        setEventHandlers(hostServices, iliasService)
 
         final Scene scene = new Scene(rootPane)
         stage = new Stage()
@@ -71,12 +79,39 @@ final class MainUi {
         menuItem.graphic = icon
     }
 
-    private void setEventHandlers(HostServices hostServices) {
+    private void setEventHandlers(HostServices hostServices, IliasService iliasService) {
         aboutAppItem.onAction = {
             try {
                 new AboutAppDialog(hostServices).show()
             } catch (IOException e1) {
-                e1.printStackTrace()
+                log.error(e1)
+            }
+        }
+        syncButton.onAction = {
+            syncButton.disable = true
+            Thread.start {
+                log.info 'running sync'
+                try {
+                    def courses = iliasService.joinedCourses.toList().blockingGet()
+                    def basePath = Paths.get '/tmp/test'
+                    def itemVisitor = new SyncingIliasItemVisitor(basePath: basePath, iliasService: iliasService)
+                    iliasService.searchCoursesWithContent(courses).subscribe(new Consumer<Course>() {
+                        @Override
+                        void accept(Course course) {
+                            itemVisitor.visit course
+                        }
+                    }, new Consumer<Throwable>() {
+                        @Override
+                        void accept(Throwable throwable) {
+                            log.error throwable
+                        }
+                    })
+                } catch (Throwable throwable) {
+                    log.error throwable
+                } finally {
+                    Platform.runLater({ syncButton.disable = false })
+                }
+                log.info 'sync finished'
             }
         }
     }
