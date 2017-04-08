75dee813968fcf255a6bcc63dd2ab309a62843f4
thetric <broj.dominik@gmail.com>
2017-04-08 15:54:02 +0200

    build(connector-domparser): replace apache http components client with okhttp3

--- connector/connector-domparser/src/main/groovy/com/github/thetric/iliasdownloader/service/webparser/impl/util/fluenthc/FluentHcExecutorFactory.groovy
+++
@@ -1,9 +0,0 @@
-package com.github.thetric.iliasdownloader.service.webparser.impl.util.fluenthc
-
-import groovy.transform.CompileStatic
-import org.apache.http.client.fluent.Executor
-
-@CompileStatic
-interface FluentHcExecutorFactory {
-    Executor createFluentHcExecutor(org.apache.http.client.CookieStore cookieStore)
-}
