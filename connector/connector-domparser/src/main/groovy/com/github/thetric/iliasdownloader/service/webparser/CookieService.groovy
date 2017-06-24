b7a135d0805cedeeaaa0c7013445a19037448ab7
thetric <broj.dominik@gmail.com>
2017-06-24 22:37:27 +0200

    refactor: delete files not belonging to `ui-cli`

--- connector/connector-domparser/src/main/groovy/com/github/thetric/iliasdownloader/service/webparser/CookieService.groovy
+++
@@ -1,9 +0,0 @@
-package com.github.thetric.iliasdownloader.service.webparser
-
-/**
- * Retrieves a cookie from a request to the specified URL.
- * The request contains <b>no authentication information</b>.
- */
-interface CookieService {
-    String getCookieFromUrl(final String url, final String cookieName) throws IOException
-}
--- connector/connector-domparser/src/main/groovy/com/github/thetric/iliasdownloader/service/webparser/impl/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Implementation of the web parser module. Do not rely on any functionality directly provided by this package as it is
- * considered non public API and is likely to change.
- */
-package com.github.thetric.iliasdownloader.service.webparser.impl;
