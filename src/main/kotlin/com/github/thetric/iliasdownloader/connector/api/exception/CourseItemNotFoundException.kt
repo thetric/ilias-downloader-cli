2a68105120fb9d7b089c9b8baf376995db4cd6d6
thetric <19861998+thetric@users.noreply.github.com>
2019-10-16 18:09:04 +0200

    refactor(connector/api): remove unused course item not found exception

--- src/main/kotlin/com/github/thetric/iliasdownloader/connector/api/exception/CourseItemNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.github.thetric.iliasdownloader.connector.api.exception
-
-/**
- * Indicates a non existing Ilias item.
- */
-class CourseItemNotFoundException(message: String, url: String) :
-    IliasException(message + url)
